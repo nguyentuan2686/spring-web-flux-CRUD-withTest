1dee6777c598795ba191d741f8f2304dfa9dfe11
nguyentuan2686 <nguyentuan2686@gmail.com>
2023-04-06 23:39:03 +0700

    "add update and delete test

--- src/test/java/com/example/springreactive/SpringReactiveMongoDbCrudApplicationTests.java
+++ src/test/java/com/example/springreactive/SpringReactiveMongoDbCrudApplicationTests.java
@@ -14,7 +14,9 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
+import static org.mockito.BDDMockito.given;
 
 @RunWith(SpringRunner.class)
 @WebFluxTest(ProductController.class)
@@ -36,6 +38,7 @@ class SpringReactiveMongoDbCrudApplicationTests {
 				.exchange()
 				.expectStatus()
 				.isOk();
+
 	}
 
 	@Test
@@ -53,5 +56,39 @@ class SpringReactiveMongoDbCrudApplicationTests {
 				.expectNext(new ProductDto("102", "mobile",1, 1000))
 				.expectNext(new ProductDto("103", "laptop",1, 10000))
 				.verifyComplete();
+	}
+
+	@Test
+	public void getProduct(){
+		Mono<ProductDto> productDtoMono = Mono.just(new ProductDto("102", "mobile",1, 1000));
+		when(service.getProduct(any())).thenReturn(productDtoMono);
+
+		Flux<ProductDto> responseBody = webTestClient.get().uri("/products/102")
+				.exchange()
+				.expectStatus().isOk()
+				.returnResult(ProductDto.class)
+				.getResponseBody();
+		StepVerifier.create(responseBody)
+				.expectSubscription()
+				.expectNextMatches(p -> p.getId().equals("102") && p.getName().equals("mobile"))
+				.verifyComplete();
+	}
+
+	@Test
+	public void updateProduct(){
+		Mono<ProductDto> productDtoMono = Mono.just(new ProductDto("102", "mobile",1, 1000));
+		when(service.updateProduct(productDtoMono, productDtoMono.blockOptional().orElseThrow(()-> new IllegalAccessError("d")).getId()))
+				.thenReturn(productDtoMono);
+		webTestClient.put().uri("/products/102")
+				.exchange()
+				.expectStatus().isOk();
+	}
+
+	@Test
+	public void deleteProduct(){
+		given(service.deleteProduct(any())).willReturn(Mono.empty());
+		webTestClient.delete().uri("/products/10")
+				.exchange()
+				.expectStatus().isOk();
 	}
 }
